ae8d29f42fa29b89d146a380cf4fdeafa1684e32
EnriqueCesar <ecesar.alsea01@gmail.com>
2026-07-13 03:13:16 -0600

    Delete src/App.tsx

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-import { DataProvider } from './components/DataContext'
-import { AppLayout } from './layouts/AppLayout'
-import { RankingPage } from './pages/RankingPage'
-
-export default function App() {
-  return <DataProvider><AppLayout><RankingPage /></AppLayout></DataProvider>
-}
